fca3eaa0788ee001f5fbd3031f323cf79a480db8
Nick Wong <nicholaswong@utexas.edu>
2020-10-19 18:05:14 -0500

    updated how Makefile and .ini are generated

--- kevm/run-kevm-specs.sh
+++ kevm/run-kevm-specs.sh
@@ -2,6 +2,8 @@
 
 VERIFIED_DIR=/mnt/extra/verified-smart-contracts
 
+THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 # extract filename if absolute path
 CONTRACT_NAME="${1##*/}"
 # default: assumes 3 vars are declared in this order
@@ -25,19 +27,22 @@ mkdir -p ${CONTRACT_DIR}
 cp ${1} ${CONTRACT_DIR}/
 
 # copy Makefile and .ini from defaults
-cp template.mak ${CONTRACT_DIR}/Makefile
-cp template.ini ${CONTRACT_DIR}/${CONTRACT_NAME%.sol}-erc20-spec.ini
+cp ${THIS_DIR}/template1.mak ${CONTRACT_DIR}/Makefile
+echo "${CONTRACT_NAME%.sol}" >> ${CONTRACT_DIR}/Makefile
+cat ${THIS_DIR}/template2.mak >> ${CONTRACT_DIR}/Makefile
+cp ${THIS_DIR}/template.ini ${CONTRACT_DIR}/${CONTRACT_NAME%.sol}-erc20-spec.ini
 
 # fill in variable locations
 echo -e "_balances: ${BALANCES_LOC}\n_allowances: ${ALLOWANCES_LOC}\n_totalSupply: ${TOTALSUPPLY_LOC}" >> ${CONTRACT_DIR}/${CONTRACT_NAME%.sol}-erc20-spec.ini
 
 # compile .sol to bytecode and insert in for "code:" in .ini
 mkdir -p ${CONTRACT_DIR}/solc_out/
-solc --bin --overwrite -o ${CONTRACT_DIR}/solc_out/ ${CONTRACT_DIR}/${CONTRACT_NAME} > /dev/null
+solc --bin-runtime --overwrite -o ${CONTRACT_DIR}/solc_out/ ${CONTRACT_DIR}/${CONTRACT_NAME} > /dev/null
 printf "code: \"0x" >> ${CONTRACT_DIR}/${CONTRACT_NAME%.sol}-erc20-spec.ini
-cat ${CONTRACT_DIR}/solc_out/${CONTRACT_SHORT_NAME}.bin >> ${CONTRACT_DIR}/${CONTRACT_NAME%.sol}-erc20-spec.ini
+cat ${CONTRACT_DIR}/solc_out/${CONTRACT_SHORT_NAME}.bin-runtime >> ${CONTRACT_DIR}/${CONTRACT_NAME%.sol}-erc20-spec.ini
 echo "\"" >> ${CONTRACT_DIR}/${CONTRACT_NAME%.sol}-erc20-spec.ini
 
 (cd ${VERIFIED_DIR};
+make -C erc20/${CONTRACT_NAME%.sol}/ clean;
 make -C erc20/${CONTRACT_NAME%.sol}/;
 make -C erc20/${CONTRACT_NAME%.sol}/ test &> erc20/${CONTRACT_NAME%.sol}/${CONTRACT_NAME%.sol}.log)
